78430638e26c55b50f79564efe539ce1154b4433
xcv58 <chenyihonglove@gmail.com>
2014-02-08 19:16:44 -0500

    change parameters;

--- comfortable_view.js
+++ comfortable_view.js
@@ -2,11 +2,15 @@
 {
     var contentWidth = 550;
     var width = window.innerWidth;
+    var marginWidth = width / 5;
     if (width / 3 >= contentWidth)
 	contentWidth = width / 3;
-    var marginWidth = width / 5;
+    if (width <= contentWidth * 1.5) {
+	marginWidth = 32;
+	contentWidth = width - 64;
+    }
     document.body.style.width = contentWidth + 'px';
     document.body.style.marginLeft = marginWidth + 'px';
-    document.body.style.fontSize = 'large';
+    document.body.style.fontSize = '19px';
     return;
 })();
